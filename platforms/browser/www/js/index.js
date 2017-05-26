af70d7987ebcdcd32c4129a2dfe6e2b9ae2d8143
ahmed <ahmednasa4@gmail.com>
2017-05-26 14:54:49 +0300

    New Theme

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -43,32 +43,52 @@ $(function() {
 
     $("#searchBtn").click(function() {
 
-        $("#loadingImg").show();
+        
 
-        var a = "%" + $("#searchTxt").val() + "%";
-        $.post("http://services.jcscc.gov.jo:8090/consumers/MarketPric.jsp", {
-                itemd: a
-            },
-            function(data, status) {
-                $("#content").html(data);
-                $("table[width='90%']").remove();
-                $("div[style='margin-top:3%;']").remove();
-                $("input[class='submit_bt']").remove();
-                $("input[type='submit']").remove();
-                $("tr[height='10%']").remove();
-                $("style").remove();
-                $('div').removeAttr('style');
-                $('table').removeAttr('style');
-                $("#searchDiv").show();
-                $("#loadingImg").hide();
-                $("table").attr("width", "100%");
-                $("table").attr({
-                    'data-role': 'table',
-                    'data-mode': 'reflow',
-                    'class': 'ui-responsive table-stroke'
+        inputTextCheck();
+
+        function inputTextCheck() {
+            var str = $("#searchTxt").val();
+
+            if (str) {
+                showResult(str);
+
+            } else {
+                alert("الرجاء ادخال اسم مادة");
+            }
+
+        }
+
+        function showResult(str) {
+            $("#loadingImg").show();
+            var replaced = str.split(' ').join('%');
+
+            var a = "%" + replaced + "%";
+            $.post("http://services.jcscc.gov.jo:8090/consumers/MarketPric.jsp", {
+                    itemd: a
+                },
+                function(data, status) {
+                    $("#content").html(data);
+                    $("table[width='90%']").remove();
+                    $("div[style='margin-top:3%;']").remove();
+                    $("input[class='submit_bt']").remove();
+                    $("input[type='submit']").remove();
+                    $("tr[height='10%']").remove();
+                    $("style").remove();
+                    $('div').removeAttr('style');
+                    $('table').removeAttr('style');
+                    $("#searchDiv").show();
+                    $("#loadingImg").hide();
+                    $("table").attr("width", "100%");
+                    $("table").attr({
+                        'data-role': 'table',
+                        'data-mode': 'reflow',
+                        'class': 'ui-responsive table-stroke'
+                    });
+
                 });
+        }
 
-            });
 
 
     });
